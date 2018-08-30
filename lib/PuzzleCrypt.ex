5b7bd0f7ba3487bfa9930aaa78b0dbfeb6e74ff7
Henry Warren <henrysdev@gmail.com>
2018-08-30 09:39:11 -0500

    init

---
+++ lib/PuzzleCrypt.ex
@@ -0,0 +1,54 @@
+defmodule Main do
+	
+	"""
+	aes encrypt a given chunk with provided password
+	"""
+	def encrypt(fragment, password) do
+		#IO.puts("[Encrypting] - password=#{password}")
+		# todo
+		fragment
+	end
+
+	"""
+	sha256 hash (aes_key + seqID)
+	"""
+	def hmac(fragment, password) do
+		sequence_id = elem(fragment, 1)
+		#IO.puts("[HMAC] - sha256(#{password} + #{sequence_id})")
+		# todo
+		fragment
+	end
+
+	"""
+	format and write out fragment to disk
+	"""
+	def write_out(fragment) do
+		sequence_id = elem(fragment, 1)
+		IO.puts("[Write-Out] - Fragment #{sequence_id}")
+		#IO.puts(List.to_string(elem(fragment, 0)))
+		IO.puts(elem(fragment, 0))
+	end
+
+	"""
+	fragment file into n parts
+	"""
+	def fragment(password, fpath, frag_count) do
+		%{size: size} = File.stat! fpath
+		chunksize = div(size, frag_count)
+		chunkrem  = rem(size, frag_count)
+		File.stream!(fpath, [], size)
+			|> Stream.chunk_every(chunksize)
+			|> Enum.map(fn(chunk) -> encrypt(chunk, password) end)
+			|> Stream.with_index
+			|> Enum.map(fn(frag) -> hmac(frag, password) end)
+			|> Enum.each(fn(frag) -> write_out(frag) end)
+	end
+
+	"""
+	reassemble n parts into single file
+	"""
+	def reassemble(password, folderpath) do
+		IO.puts("reassemble")
+	end
+
+end
