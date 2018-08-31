e707e40d6d5d0741f5f8542f3c2e8c10fe9c4c4a
Henry Warren <henrysdev@gmail.com>
2018-08-30 20:38:48 -0500

    made merge algorithm much more elegant + efficient + changed comment format to avoid warnings

--- lib/PuzzleCrypt.ex
+++ lib/PuzzleCrypt.ex
@@ -1,17 +1,13 @@
-defmodule Main do
+defmodule PuzzleCrypt do
 
-	"""
-	aes encrypt a given chunk with provided password
-	"""
+	# aes encrypt a given chunk with provided password
 	def encrypt(fragment, password) do
 		IO.puts("[Encrypting] - password=#{password}")
 		# todo
 		fragment
 	end
 
-	"""
-	sha256 hash (aes_key + seqID)
-	"""
+	# sha256 hash (aes_key + seqID)
 	def hmac(fragment, password) do
 		sequence_id = elem(fragment, 1)
 		IO.puts("[HMAC] - sha256(#{password} + #{sequence_id})")
@@ -19,48 +15,38 @@ defmodule Main do
 		fragment
 	end
 
-	"""
-	format and write out fragment to disk
-	"""
+	# format and write out fragment to disk
 	def write_out(fragment) do
 		sequence_id = elem(fragment, 1)
 		IO.puts("[Write-Out] - Fragment #{sequence_id}")
-		#IO.puts(List.to_string(elem(fragment, 0)))
 		IO.puts(elem(fragment, 0))
+		# todo
 	end
 
-	"""
-	evenly distribute file bytes over fragments
-	"""
-	def compare_and_merge(h, h1, frag_count, frag_count), do: [ h <> h1, [ ] ]
-	def compare_and_merge(_h, _h1, _, _), do: [ _h, _h1 ]
-
-	def merge_remainder([ _h ], _, _), do: [ _h ]
-	def merge_remainder([ k | _tail ], count, frag_count) do
-		[ h, h1 | _t ] = [ k | _tail]
-		[ h , _h1 ] = compare_and_merge(h, h1, count, frag_count)
-		[ h ] ++ merge_remainder([ _h1 | _t ], count + 1, frag_count)
+	# evenly distribute file bytes over fragments
+	def merge_rem(remainders, last, last) do
+		[Enum.join(remainders)]
+	end
+	def merge_rem([ head | _tail ], count, frag_count) do
+		[ new_head | _new_tail ] = _tail
+		[ head ] ++ merge_rem([ new_head | _new_tail ], count + 1, frag_count)
 	end
 
-	"""
-	fragment file into n parts
-	"""
+	# fragment file into n parts
 	def fragment(password, fpath, frag_count) do
 		%{size: size} = File.stat! fpath
+		# todo: throw error if frag_count > size
 		chunksize = div(size, frag_count)
 		File.stream!(fpath, [], chunksize)
 			|> Enum.to_list
-			|> merge_remainder(1, frag_count)
-			|> Enum.filter(& &1 != [ ])
+			|> merge_rem(1, frag_count)
 			|> Enum.map(fn(chunk) -> encrypt(chunk, password) end)
 			|> Stream.with_index
 			|> Enum.map(fn(frag) -> hmac(frag, password) end)
 			|> Enum.each(fn(frag) -> write_out(frag) end)
 	end
 
-	"""
-	reassemble n parts into single file
-	"""
+	# reassemble n parts into single file
 	def reassemble(password, folderpath) do
 		IO.puts("reassemble")
 	end
